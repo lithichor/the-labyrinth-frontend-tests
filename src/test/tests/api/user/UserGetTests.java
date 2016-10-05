a60192ebdd257c5cca1d6cff33573372b5c1df87
Eric Hartill <euclidsieve@gmail.com>
2016-10-05 16:15:46 -0400

    created full regression suite xml

--- src/test/tests/api/user/UserGetTests.java
+++ src/test/tests/api/user/UserGetTests.java
@@ -14,7 +14,7 @@ import test.parents.LabyrinthAPITest;
 public class UserGetTests extends LabyrinthAPITest
 {
 
-	@Test(groups = {"full_regression", "get"})
+	@Test
 	public void getCurrentUser()
 	{
 		HttpGet get = makeGetMethod("user");
