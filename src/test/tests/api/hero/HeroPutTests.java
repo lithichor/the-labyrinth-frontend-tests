83df764049e19cc4180f7dd997f14a21aab6298c
Eric Hartill <euclidsieve@gmail.com>
2016-11-09 18:13:28 -0500

    started on tests for hero -PUT

---
+++ src/test/tests/api/hero/HeroPutTests.java
@@ -0,0 +1,17 @@
+package test.tests.api.hero;
+
+import org.testng.annotations.BeforeTest;
+
+import test.parents.LabyrinthAPITest;
+
+public class HeroPutTests extends LabyrinthAPITest
+{
+//	private HeroClient client;
+
+	@BeforeTest
+	public void setup()
+	{
+		System.out.println("STARTNG USER PUT TESTS");
+		
+	}
+}
