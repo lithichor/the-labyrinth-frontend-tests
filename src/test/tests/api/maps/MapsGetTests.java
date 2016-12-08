5cc4fbc59a125c365ec94cc8f0e4e20c9f25ca7a
Eric Hartill <euclidsieve@gmail.com>
2016-12-08 14:38:05 -0500

    added test to get map for deleted game

--- src/test/tests/api/maps/MapsGetTests.java
+++ src/test/tests/api/maps/MapsGetTests.java
@@ -38,4 +38,23 @@ public class MapsGetTests extends LabyrinthAPITest
 		Assert.assertEquals(mapIdFromGame, mapId,
 				"The IDs do not match:\nFrom Game object: " + mapIdFromGame + "\nFrom Map object: " + mapId);
 	}
+	
+	@Test
+	public void getMapsForDeletedGame()
+	{
+		// create a game
+		String game = gamesClient.createGame();
+		JsonObject gameObj = gson.fromJson(game, JsonObject.class);
+		int gameId = gameObj.get("id").getAsInt();
+		// delete the game
+		gamesClient.deleteGame(gameId);
+		
+		// get the maps for the game
+		String maps = mapsClient.getMapsForGame(gameId);
+		
+		Assert.assertTrue(maps.contains("We could not find a Map for that Game ID"),
+				"We should have gotten an error message, but instead got this:\n" +
+				maps);
+		
+	}
 }
