4e0b9c3b1cd05bf0cdd6906b49a9d0cff78577e6
Eric Hartill <euclidsieve@gmail.com>
2016-12-08 17:16:07 -0500

    created new tests to get maps with invalid IDs

--- src/test/tests/api/maps/MapsGetTests.java
+++ src/test/tests/api/maps/MapsGetTests.java
@@ -29,14 +29,18 @@ public class MapsGetTests extends LabyrinthAPITest
 		JsonObject gameObj = gson.fromJson(game, JsonObject.class);
 		JsonArray mapsFromGame = gameObj.get("mapIds").getAsJsonArray();
 		int mapIdFromGame = mapsFromGame.get(0).getAsInt();
+		int gameIdFromGame = gameObj.get("id").getAsInt();
 		
 		//get maps for the game
 		String map = mapsClient.getCurrentGameMaps();
 		JsonObject mapObj = gson.fromJson(map, JsonObject.class);
 		int mapId = mapObj.get("id").getAsInt();
+		int gameIdFromMap = mapObj.get("gameId").getAsInt();
 		
 		Assert.assertEquals(mapIdFromGame, mapId,
 				"The IDs do not match:\nFrom Game object: " + mapIdFromGame + "\nFrom Map object: " + mapId);
+		Assert.assertEquals(gameIdFromGame, gameIdFromMap,
+				"The IDs do not match:\nFrom Game object: " + gameIdFromGame + "\nFrom Map object: " + gameIdFromMap);
 	}
 	
 	@Test
@@ -55,6 +59,46 @@ public class MapsGetTests extends LabyrinthAPITest
 		Assert.assertTrue(maps.contains("We could not find a Map for that Game ID"),
 				"We should have gotten an error message, but instead got this:\n" +
 				maps);
+	}
+	
+	@Test
+	public void getMapUsingBogusMapId()
+	{
+		// create a game
+		String game = gamesClient.createGame();
+		JsonObject gameObj = gson.fromJson(game, JsonObject.class);
 		
+		// try to get the map with bogus string id
+		// it should return the map
+		String map = mapsClient.getMapsForGame("this_is_bogus");
+		JsonObject mapObj = gson.fromJson(map, JsonObject.class);
+		
+		Assert.assertEquals(gameObj.get("id").getAsInt(),
+				mapObj.get("gameId").getAsInt(),
+				"The game IDs do not match\nGame: " + game + "\nMap: " + map);
+		Assert.assertEquals(gameObj.get("mapIds").getAsJsonArray().get(0).getAsInt(),
+				mapObj.get("id").getAsInt(),
+				"The map IDs do not match\nGame: " + game + "\nMap: " + map);
+	}
+	
+	@Test
+	public void getMapUsingInvalidId()
+	{
+		// create a game
+		gamesClient.createGame();
+		
+		// try to get the map using various methods with
+		// and invalid ID
+		String map1 = mapsClient.getMapsForGame(10);
+		String map2 = mapsClient.getMapsForGame("10");
+		String map3 = mapsClient.getMapsFromMapId(10);
+		String map4 = mapsClient.getMapsFromMapId("10");
+		
+		String message = "We could not find a Map for that Game ID";
+		
+		Assert.assertTrue(map1.contains(message));
+		Assert.assertTrue(map2.contains(message));
+		Assert.assertTrue(map3.contains(message));
+		Assert.assertTrue(map4.contains(message));
 	}
 }
