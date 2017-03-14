2166264081240479c85a84033a3ac1a009a600ad
Eric Hartill <euclidsieve@gmail.com>
2017-03-14 17:36:26 -0400

    #109 - added tests for invalid IDs

--- src/test/tests/api/turns/TurnsGetTests.java
+++ src/test/tests/api/turns/TurnsGetTests.java
@@ -88,4 +88,128 @@ public class TurnsGetTests extends TurnsApiTest
 				+ "\nResponse from Server: " + secondTurn
 				+ "Expected Message: " + message + "\n**");
 	}
+
+	@Test
+	public void getTurnWithNoIdString()
+	{
+		String turn = turnsClient.getTurn("");
+		String message = "There is no turn matching that ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+	
+	@Test
+	public void getTurnWithNullIdString()
+	{
+		String nullStr = null;
+		String turn = turnsClient.getTurn(nullStr);
+		String message = "There is no turn matching that ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+	
+	@Test
+	public void getTurnWithNoIdInteger()
+	{
+		String turn = turnsClient.getTurn(0);
+		String message = "There is no turn matching that ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+
+	@Test
+	public void getTurnWithNullIdInteger()
+	{
+		Integer integer = null;
+		String turn = turnsClient.getTurn(integer);
+		String message = "There is no turn matching that ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+
+	@Test
+	public void getTurnWithInvalidIdString()
+	{
+		String turn = turnsClient.getTurn("qweasd");
+		String message = "There is no turn matching that ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+	
+	@Test
+	public void getTurnWithInvalidIdInteger()
+	{
+		String turn = turnsClient.getTurn(-2);
+		String message = "There is no turn matching that ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+
+	@Test
+	public void getTurnFromTileWithNoIdString()
+	{
+		String turn = turnsClient.getTurnForGame("");
+		String message = "There is no turn matching that Game ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+	
+	@Test
+	public void getTurnFromTileWithNullIdString()
+	{
+		String nullStr = null;
+		String turn = turnsClient.getTurnForGame(nullStr);
+		String message = "There is no turn matching that Game ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+	
+	@Test
+	public void getTurnFromTileWithNoIdInteger()
+	{
+		String turn = turnsClient.getTurnForGame(0);
+		String message = "There is no turn matching that Game ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+
+	@Test
+	public void getTurnFromTileWithNullIdInteger()
+	{
+		Integer integer = null;
+		String turn = turnsClient.getTurnForGame(integer);
+		String message = "There is no turn matching that Game ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+
+	@Test
+	public void getTurnFromTileWithInvalidIdString()
+	{
+		String turn = turnsClient.getTurnForGame("qweasd");
+		String message = "There is no turn matching that Game ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:\n"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
+	
+	@Test
+	public void getTurnFromTileWithInvalidIdInteger()
+	{
+		String turn = turnsClient.getTurnForGame(-2);
+		String message = "There is no turn matching that Game ID";
+		assertTrue(turn.contains(message), "Did not get the expected response:"
+				+ "\nExpected: " + message
+				+ "\nRecieved: " + turn);
+	}
 }
